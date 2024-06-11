0808b96118a6530fc6a9e15b3f30831877e7ac2c
Istvan Molnar <i.molnar@reply.de>
2024-06-11 14:36:52 +0200

    api folder moved

---
+++ src/app/(authenticated)/layout.tsx
@@ -0,0 +1,28 @@
+import { AuthenticatedProviders } from "@/features/globals/providers";
+import { MainMenu } from "@/features/main-menu/main-menu";
+import { AI_NAME } from "@/features/theme/theme-config";
+import { cn } from "@/ui/lib";
+
+export const dynamic = "force-dynamic";
+
+export const metadata = {
+  title: AI_NAME,
+  description: AI_NAME,
+};
+
+export default function RootLayout({
+  children,
+  params: { locale },
+}: {
+  children: React.ReactNode;
+  params: { locale: 'de' | 'en' };
+}) {
+  return (
+    <AuthenticatedProviders>
+      <div className={cn("flex flex-1 items-stretch")}>
+        <MainMenu {...{locale}}/>
+        <div className="flex-1 flex">{children}</div>
+      </div>
+    </AuthenticatedProviders>
+  );
+}
